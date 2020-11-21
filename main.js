c2ce3de316847ed860e4e162c4c5fb4f16b7907a
Sukesh96 <50539059+Sukesh96@users.noreply.github.com>
2020-11-21 18:07:31 +0530

    New Update 21-11-2020

--- main.js
+++ main.js
@@ -1,22 +1,91 @@
-let mobileNav = document.querySelector(".mobile-nav");
-let toggleBtn = document.querySelector(".toggle_button");
-let backdrop = document.querySelector(".backdrop");
-
-
-toggleBtn.addEventListener("click",function(){
-    mobileNav.style.display = "block";
-    backdrop.style.display = "block";
-});
-
-backdrop.addEventListener("click",function(){
-    mobileNav.style.display = "none";
-    backdrop.style.display = "none";
-});
-
-
-//Pre Loader
+//  Pre-Loader
 let preLoader = document.querySelector(".preloader");
 
 function myLoader(){
     preLoader.style.display ="none";
+}
+
+// Toggle Button
+$(document).ready(function(){
+    $(".toggle_button").click(function(){
+        $(".mobile-nav").toggle(500);
+        $(".backdrop").toggle();
+    });
+
+    $(".backdrop").click(function(){
+        $(".mobile-nav").toggle(500);
+        $(".backdrop").toggle();
+    });
+});
+
+// Type Writer Effect
+const TypeWriter = function(txtElement, words, wait){
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+
+//  Type Method
+TypeWriter.prototype.type = function(){
+    //  Current Index Of Word
+    const current = this.wordIndex % this.words.length;
+
+    //  Get Full Text Of Current Index
+    const fullTxt = this.words[current];
+
+    if(this.isDeleting){
+        //  Remove Char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    }
+    else{
+        // Add Char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);   
+    }
+
+    //  Insert Text Into Element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    //  Initial Type Speed
+    let typeSpeed = 100;
+    if(this.isDeleting){
+        typeSpeed = typeSpeed / 2;
+    }
+
+    //  If word is complete
+    if(!this.isDeleting && this.txt === fullTxt){
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+    }
+    else if(this.isDeleting && this.txt === ''){
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 100;
+    }
+
+    setTimeout(() => {
+        this.type();
+    },typeSpeed);
+}
+
+//  Init On DOM Load
+document.addEventListener("DOMContentLoaded",init);
+
+//  Init App
+
+function init(){
+    const txtElement = document.querySelector(".txt-type");
+    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    const wait = txtElement.getAttribute("data-wait");
+
+    //  Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
 }
